10b06019376cfd1bb3faf24985db7fe984bce4b9
Biswajeet mishra <100258351+jeet4747@users.noreply.github.com>
2024-10-11 19:09:40 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,36 @@
+"use stirct";
+
+// alert(3 + 3) // we are using node js not browser
+
+console.log(3 + 3) | console.log("jeet mishra"); // this is wrong way to write this has no readablity
+
+console.log("jeet mishra")
+console.log(3 + 3)
+// this is right way to write code this easy to read for readables and revision
+
+
+let name = "jeet";
+let age = 18;
+let percentage = "50%"
+let isloggedin = true
+
+
+// this are some datatypes //
+// number => 2 to power
+// bigint 
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => this is what when we not define any value 
+// symbol => unique
+
+
+// object
+
+
+
+
+
+
+console.log(typeof("hello")) // this is how are know the type of that value
+console.log(typeof undefined) 
