9d9d76f04ae2a4ad7fc7f51655bd637b1c2b43d0
Biswajeet mishra <100258351+jeet4747@users.noreply.github.com>
2024-10-11 20:00:05 +0000

    converstion about Oprations and coverts into another datatypes

---
+++ 01_basic/03_convertionOperations.js
@@ -0,0 +1,26 @@
+let score = "hitesh";
+
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueinnumber = Number(score)
+// console.log(typeof(valueinnumber));
+// console.log(valueinnumber);
+
+
+// "33" this converts easing 
+// "33abc" => NAN
+// true => 1; False => 0
+
+let isloggedin = 1
+
+let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin)
+
+
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+// console.log(typeof(stringnumber));
+// console.log(stringnumber);
