75a85562b6cb813b79e370ce71a53b3d529a8e01
Biswajeet mishra <100258351+jeet4747@users.noreply.github.com>
2024-10-11 18:50:36 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountID = 123445
+let accountEmail = "hitesh@gmail.com"
+var accountPassword = "12345" // prefer to not use too much Varf
+accountcity = "jaipur"
+
+// accountID = 2 this is not allowed (this is constant variable)
+
+accountEmail = "mrjeet088@gmail.com"
+accountPassword = "47474747"
+accountcity = "nashik"
+
+console.log(accountID);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(typeof(accountcity), accountcity);
+
+console.table([accountID, accountEmail, accountPassword, accountcity])
