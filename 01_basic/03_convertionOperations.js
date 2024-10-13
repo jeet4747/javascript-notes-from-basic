49a7a5cb649f548dd858376b514080d4df30f46f
Biswajeet mishra <100258351+jeet4747@users.noreply.github.com>
2024-10-13 21:12:02 +0000

    more detailed explained for Author only

--- 01_basic/03_convertionOperations.js
+++ 01_basic/03_convertionOperations.js
@@ -24,3 +24,54 @@ let somenumber = 33
 let stringnumber = String(somenumber)
 // console.log(typeof(stringnumber));
 // console.log(stringnumber);
+
+
+
+
+// *************************** Operations ***************************
+
+let value = 3
+let negvalue = -value
+
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+let str1 = "hitesh "
+let str2 = "hello"
+
+
+let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log("1" + 2)
+// console.log("1" + 2 + "2")
+// console.log("1" + "2")
+
+// console.log(+true) we do not write code in this format
+
+
+// let num1, num2, num3
+
+// num1 = num3 = num2 = 2+2
+
+
+// let gamecounter = 100
+// gamecounter++;
+// console.log(gamecounter)
+
+
+
+
+
+
+// link for study to increment (postfix & prefix)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
