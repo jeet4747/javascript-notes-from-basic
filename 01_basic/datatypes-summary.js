33e07d7de5148fe173d45d0daba0f6062847b1d1
Biswajeet mishra <100258351+jeet4747@users.noreply.github.com>
2024-10-13 21:45:01 +0000

    datatype-summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,55 @@
+// # Primitive Datatype
+// 7 Types = [String, Number, Boolean, Null, Undefined, Symbol, BigInt] 
+// String = ""
+// Number = 123456
+// Boolean = True/False
+// Null
+// Undefined 
+// Symbol = $#^
+BigInt = 75647658756
+// console.log(typeof(BigInt));
+
+
+
+
+
+// # Refrence Type (Non-Primitive)
+// Arrays, Objects, Functions,  
+
+//Array
+const heros = ["spiderman", "doga", "naagraj"]
+
+
+//Objects
+const heross = {
+    name:"jeet",
+    age:22
+
+}
+
+
+// Function
+const myfunct = ()=>{
+    console.log("hello World");
+    
+}
+
+
+
+
+// detailed expample
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
