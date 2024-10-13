1c887e0b8fa3a62183d2d408d60825dfb6849ec7
Biswajeet mishra <100258351+jeet4747@users.noreply.github.com>
2024-10-13 21:21:23 +0000

    Comparition

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,32 @@
+        // ******************************This is Example of comparition*******************************
+
+
+        // this is basic comparition 
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 = 1)
+// console.log(2 == 1);
+// console.log(2 != 1)
+
+
+
+
+
+
+
+// this is boolean Comparition
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+
+
+//this is Advance Comparition
+// console.log(null > 0)
+// console.log(null == 0);
+// console.log(null >= 0); 
+
+// console.log(undefined == 0)
+// console.log(undefined > 0 );
+// console.log(undefined < 0 );
+
+// console.log("2" === 2);
