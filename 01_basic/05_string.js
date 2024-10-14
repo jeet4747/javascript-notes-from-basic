f0b199ace690e9eb142bc2e24d5c13fd972f8fff
Biswajeet mishra <100258351+jeet4747@users.noreply.github.com>
2024-10-14 22:15:49 +0000

    incomplete going to comple tommorow

---
+++ 01_basic/05_string.js
@@ -0,0 +1,21 @@
+const name = "hitesh"
+const repocount = 50
+
+//console.log(name + repocount + `value`)
+
+console.log(`hello how are you, my name is ${name} and i have ${repocount} reposotory`);
+
+const gameName = new String('Jeet mishra')
+
+console.log(gameName[3]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.concat(5));
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(6));
+
+
+gameName.slice
+console.log()
+
