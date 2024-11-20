544f1d93544fca8b0f44b082fa0fa00f24b0b3c6
Biswajeet mishra <100258351+jeet4747@users.noreply.github.com>
2024-11-20 21:04:58 +0000

    pending not completed

---
+++ 05_iteration/six.js
@@ -0,0 +1,15 @@
+// const coding = ["js", "ruby", "java", "python", "cpp", "rust"]
+
+// const value = coding.forEach((item)=>{
+//     //console.log(item);
+//     return item;
+// })
+
+// console.log(value)
+
+const mynums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newnums = mynums.filter( (nums)=> {
+    nums > 4
+} )
+console.log(newnums)
